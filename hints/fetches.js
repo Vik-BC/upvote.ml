8559d7b3bda78cc5eb129bc86291da743e565ae0
vik <v.lateralus@gmail.com>
2019-08-09 22:30:47 +0300

    Update fetches.js

--- hints/fetches.js
+++ hints/fetches.js
@@ -25,3 +25,16 @@ fetch("http://116.203.104.164:8888/v1/chain/get_abi", {
   .then(function(myJson) {
     console.log(JSON.stringify(myJson));
   });
+
+fetch("http://116.203.104.164:8888/v1/chain/get_table_rows", {
+  body: '{"scope":"gls.publish", "code":"gls.publish", "table":"message", "index":"primary"}',
+  headers: {
+    "Content-Type": "application/json"
+  },
+  method: "POST"
+}).then(function(R) {
+    return R.json();
+  })
+  .then(function(X) {
+    console.log(JSON.stringify(X));
+  });
