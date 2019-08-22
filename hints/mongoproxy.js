25e8236db34b0de2a2b7bffeefb1185a397963dd
vik <v.lateralus@gmail.com>
2019-08-22 14:56:37 +0300

    Get GOLOS name from gls domain

--- hints/mongoproxy.js
+++ hints/mongoproxy.js
@@ -1,16 +1,13 @@
-const cybername = "rtvmqvz3i1vt"
-const assert = require('assert')
-const url = 'mongodb://*****.mongo.golos.cf:000000'
-const dbName = '_CYBERWAY_'
-
 const MO = require('mongodb').MongoClient
-MO.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
-.then((client)=>{
-  const db = client.db(dbName);
-  const col = db.collection('username');
-  col.find({owner:cybername,scope:'gls'}).toArray((err, gls)=>{
-    assert.equal(err, null)
-    console.log(gls[0].name)
-    client.close();
-  })
-})
+const url = 'mongodb://********.io:*****'
+const dbName = '_CYBERWAY_'
+async function nameCW2GLS(cybername) {
+        const client = await MO.connect(url, {useNewUrlParser: true, useUnifiedTopology: true})
+        const db = await client.db(dbName);
+        const col = await db.collection('username');
+        const USER = await col.find({owner:cybername,scope:'gls'}).toArray()
+        if(USER[0]&&USER[0].name) return USER[0].name;
+        return ' ERROR GETTING YOUR NAME FROM GOLOS';
+        
+         }
+module.exports.nameCW2GLS = nameCW2GLS
